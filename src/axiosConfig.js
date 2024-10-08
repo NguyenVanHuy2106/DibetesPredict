779090a9d1dbf7c037de1c7105771590ee208111
Nguyễn Văn Huy <77230707+NguyenVanHuy2106@users.noreply.github.com>
2024-10-08 16:22:40 +0700

    Downgrade Node.js to 16.x

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Tạo instance của axios
 const axiosInstance = axios.create({
-  baseURL: "https://api-predict.tinakids.vn/", // Domain API mới
+  baseURL: "https://api-predict.tinakids.vn/api/", // Domain API mới
   timeout: 300000, // Thời gian chờ request
 });
 
